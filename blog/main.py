01ea8891dfd8726f1e868b5c422035d0a419ef19
edwinogwel <edogwel@gmail.com>
2025-07-17 00:47:50 +0300

    Add relationship

--- blog/main.py
+++ blog/main.py
@@ -21,7 +21,7 @@ def get_db():
 
 @app.post("/blog", response_model=schemas.ShowBlog, tags=["blogs"])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
